ca4b3be5b48ef61d80b600c412fb6731910e427d
Moliari <danielmoliaribarbosa@gmail.com>
2024-09-05 23:12:06 -0300

    fix: Ajusta checkbox e aparição de nodes "from" - Versão 3.1.1

--- components/TopologicalSort.tsx
+++ components/TopologicalSort.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { IGraph } from '@/interfaces';
 import GraphDisplay from './GraphDisplay';
 
@@ -11,77 +11,168 @@ const TopologicalSort: React.FC<TopologicalSortProps> = ({ graph }) => {
   const [nodeTimes, setNodeTimes] = useState<{
     [key: string]: [number, number];
   }>({});
-
+  const [availableNodes, setAvailableNodes] = useState<string[]>([]);
+  const [selectedNode, setSelectedNode] = useState<string | null>(null);
   const cyRef = useRef<cytoscape.Core | null>(null);
 
+  let time = useRef(0); // Controla o tempo globalmente
+
+  // Inicializa a lista de nós 'from'
+  useEffect(() => {
+    console.log('Effect triggered. Initializing available nodes...');
+    initializeAvailableNodes();
+  }, [graph]);
+
+  // Função que inicializa nós 'from'
+  const initializeAvailableNodes = () => {
+    console.log('Initializing available nodes...');
+    const fromNodes = new Set(graph.edges.map((edge) => edge.from));
+
+    console.log('From nodes:', Array.from(fromNodes));
+
+    // Define os nós disponíveis como apenas os 'from'
+    setAvailableNodes(Array.from(fromNodes));
+  };
+
+  // Função que realiza a ordenação topológica
   const topologicalSort = () => {
-    let time = 0;
+    if (!selectedNode) {
+      alert('Selecione um nó de partida.');
+      return;
+    }
+
+    console.log('Starting topological sort with node:', selectedNode);
+
     const visited: { [key: string]: boolean } = {};
     const times: { [key: string]: [number, number] } = {};
     const result: string[] = [];
 
     const dfs = (node: string) => {
+      console.log(`Visiting node: ${node}`);
+      if (visited[node]) return; // Se já foi visitado, saia da função
       visited[node] = true;
-      times[node] = [++time, 0];
+      times[node] = [++time.current, 0];
 
       const neighbors = graph.edges
         .filter((edge) => edge.from === node)
         .map((edge) => edge.to);
+
+      console.log(`Neighbors of node ${node}:`, neighbors);
+
       neighbors.forEach((neighbor) => {
-        if (!visited[neighbor]) dfs(neighbor);
+        if (!visited[neighbor]) {
+          dfs(neighbor);
+        }
       });
 
-      times[node][1] = ++time;
+      times[node][1] = ++time.current;
       result.push(node);
+
+      console.log(`Finished visiting node: ${node}`);
     };
 
-    graph.edges.forEach((edge) => {
-      if (!visited[edge.from]) dfs(edge.from);
-    });
+    // Reinicializa o set de nós visitados para cada nova execução
+    dfs(selectedNode);
 
-    setSortedNodes(result.reverse());
-    setNodeTimes(times);
+    console.log('DFS complete. Result:', result);
+    console.log('Visited nodes:', visited);
+    console.log('Times:', times);
+
+    // Atualiza os nós disponíveis que ainda não foram visitados
+    const remainingNodesWithoutDependencies = availableNodes.filter(
+      (node) => !visited[node],
+    );
+    console.log(
+      'Remaining nodes without dependencies:',
+      remainingNodesWithoutDependencies,
+    );
+
+    setAvailableNodes(remainingNodesWithoutDependencies);
+
+    // Adiciona os nós ordenados e os tempos de descoberta/conclusão
+    setSortedNodes((prevSortedNodes) => [
+      ...prevSortedNodes,
+      ...result.reverse(),
+    ]);
+    setNodeTimes((prevNodeTimes) => ({ ...prevNodeTimes, ...times }));
+  };
+
+  // Lida com a seleção de um nó
+  const handleNodeSelection = (node: string) => {
+    console.log('Node selected:', node);
+    setSelectedNode(node);
   };
 
   return (
     <div className="p-4">
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4"
-        onClick={topologicalSort}
-      >
-        Ordenar
-      </button>
       <div className="flex flex-col lg:flex-row gap-6">
         <div className="lg:w-[30%] flex flex-col space-y-4">
-          {sortedNodes.length > 0 && (
-            <h3 className="font-semibold">Ordem Topológica:</h3>
+          <h3 className="font-semibold mb-4">Selecione um nó de partida:</h3>
+          {availableNodes.length > 0 ? (
+            <ul className="flex flex-row gap-3 flex-wrap mb-4">
+              {availableNodes.map((node) => (
+                <li key={node}>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      checked={selectedNode === node}
+                      onChange={() => handleNodeSelection(node)}
+                    />
+                    <span>{node}</span>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-red-500">
+              Não há nós disponíveis para selecionar.
+            </p>
           )}
-          <p>{sortedNodes.join(' -> ')}</p>
+
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4"
+            onClick={topologicalSort}
+            disabled={!selectedNode}
+          >
+            Ordenar
+          </button>
+
           {sortedNodes.length > 0 && (
-            <h3 className="font-semibold mt-4">
-              Tempos de Descoberta e Finalização:
-            </h3>
+            <>
+              <h3 className="font-semibold">Ordem Topológica:</h3>
+              <p>{sortedNodes.join(' -> ')}</p>
+            </>
           )}
-          <ul className="list-disc list-inside space-y-2 max-h-96 overflow-y-auto">
-            {Object.entries(nodeTimes).map(([node, times]) => (
-              <li
-                key={node}
-                className="bg-gray-100 p-3 rounded-lg shadow-sm border border-gray-300 flex items-center justify-between"
-              >
-                <span className="font-medium text-gray-700">{node}</span>
-                <span className="text-gray-500">
-                  ({times[0]}/{times[1]})
-                </span>
-              </li>
-            ))}
-          </ul>
+
+          {sortedNodes.length > 0 && (
+            <>
+              <h3 className="font-semibold mt-4">
+                Tempos de Descoberta e Finalização:
+              </h3>
+              <ul className="list-disc list-inside space-y-2 max-h-64 overflow-y-auto">
+                {Object.entries(nodeTimes).map(([node, times]) => (
+                  <li
+                    key={node}
+                    className="bg-gray-100 p-3 rounded-lg shadow-sm border border-gray-300 flex items-center justify-between"
+                  >
+                    <span className="font-medium text-gray-700">{node}</span>
+                    <span className="text-gray-500">
+                      ({times[0]}/{times[1]})
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
+
         <div className="lg:w-[70%] bg-gray-100 border border-gray-300 rounded-lg p-4 flex-grow">
           <GraphDisplay
             edges={graph.edges}
             graphType="directional"
             nodeTimes={nodeTimes}
             onGraphInit={(cy) => {
+              console.log('Graph initialized');
               cyRef.current = cy;
             }}
           />
